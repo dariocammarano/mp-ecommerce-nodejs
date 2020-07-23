71369b6a774f3b480b5b522e622507a06290dead
Dario Cammarano <dari_roj@hotmail.com>
2020-07-23 00:32:55 -0300

    v1.20

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ res.render('detail', req.query);
 router.get('/payment/success', controller.getBackUrlSuccess);
 router.get('/payment/failure', controller.getBackUrlFailure);
 router.get('/payment/pending', controller.getBackUrlPending);
-router.get('/payment/webhook', controller.getWebHook);
+router.post('/payment/webhook', controller.getWebHook);
 router.get('/hook', controller.getHook);
 
 router.post('/pagar', controller.create);
